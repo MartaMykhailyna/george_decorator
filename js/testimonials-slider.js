eacf2cd6767347cfcc75ef0cec668cc8f44fdc99
MartaMykhailyna <marta.mykhailyna-pi2002@kep.nung.edu.ua>
2023-10-06 01:19:42 +0300

    change hospitals, schools, to do: footer pages hospitals and schools(their content)

--- js/testimonials-slider.js
+++ js/testimonials-slider.js
@@ -3,19 +3,27 @@ const swiper = new Swiper('.swiper', {
     direction: 'vertical',
     loop: true,
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
+
     },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+    injectStyles: [
+        `
+          .swiper-button-next,
+          .swiper-button-prev {
+            background-color: white;
+            padding: 8px 16px;
+            border-radius: 100%;
+            border: 2px solid black;
+            color: red;
+          }
+          .swiper-pagination-bullet{
+            width: 40px;
+            height: 40px;
+            background-color: red;
+          }
+      `,
+    ],
+})
