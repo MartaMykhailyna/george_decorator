bf66e6c684843ac49939da5ff3796231322300a3
MartaMykhailyna <marta.mykhailyna-pi2002@kep.nung.edu.ua>
2023-10-08 00:16:25 +0300

    df

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,37 +1,143 @@
-(function () {
-    const form = document.querySelector("#contact-form-formspree");
-    form.addEventListener("submit", handleSubmit);
-  
-    function handleSubmit(event) {
-      event.preventDefault();
-      const status = document.getElementById("contact-form-status");
-      fetch(event.target.action, {
-        method: form.method,
-        body: new FormData(event.target),
-        headers: {
-          Accept: "application/json",
-        },
-      })
-        .then((response) => {
-          if (response.ok) {
-            status.innerHTML = "Thanks for your submission!";
-            form.reset();
-          } else {
-            response.json().then((data) => {
-              if (Object.hasOwn(data, "errors")) {
-                status.innerHTML = data["errors"]
-                  .map((error) => error["message"])
-                  .join(", ");
-              } else {
-                status.innerHTML =
-                  "Oops! There was a problem submitting your form";
-              }
+"use strict"
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', formSend);
+    async function formSend(e) {
+        e.preventDefault();
+
+        let error = formValidate(form); 
+        let formData = new FormData(form);
+
+        if (error === 0){
+          form.classList.add('_sending');
+          let response = await fetch ('sendmail.php',{
+              method: 'POST',
+              body: formData
             });
-          }
-        })
-        .catch((error) => {
-          status.innerHTML = "Oops! There was a problem submitting your form";
-        });
-    }
-  
-  })();
+            if(response.ok){
+                let result = await response.json();
+                alert(result.message);
+                formPreview.innerHTML = '';
+                form.reset();
+                form.classList.remove('_sending');
+            }
+            else{
+              alert('Problem');
+              form.classList.remove('_sending');
+            }
+        } else{
+          alert("заповніть поля")
+        } 
+      }
+
+      function formValidate ( form ) {
+        let error = 0 ;
+        let formReq = document.querySelectorAll ('._req');
+        for ( let index = 0 ; index < formReq.length ; index ++ ) {
+            const input = formReq[index];
+            formRemoveError ( input ) ;
+            if (input.classList.contains('_email')) { 
+                if(emailTest(input)){
+                  formAddError(input);
+                  error++;
+                }
+              } else if(input.getAttribute("type")==="checkbox" && input.checked === false){
+                formAddError(input);
+                error++;
+              }  else{
+                  if (input.value ===''){
+                    formAddError(input);
+                    error++;
+                  }
+                }}
+                return error;
+        }
+     
+      function formAddError ( input ) {
+        input.parentElement.classList.add ( ' _error ' ) ;
+        input.classList.add ( ' _error ' ) ;
+      }
+      function formRemoveError ( input ) {
+        input.parentElement.classList.remove ( ' _error ' ) ;
+        input.classList.remove ( ' _error ' ) ;
+      }
+      // Функция места email
+      function emailTest ( input ) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test( input.value );
+        }
+}); 
+
+
+// "use strict";
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const form = document.getElementById('form');
+//     const formPreview = document.querySelector('.form_preview');
+//     form.addEventListener('submit', formSend);
+
+//     async function formSend(e) {
+//         e.preventDefault();
+
+//         let error = formValidate(form);
+//         let formData = new FormData(form);
+
+//         if (error === 0) {
+//             form.classList.add('_sending');
+//             let response = await fetch('sendmail.php', {
+//                 method: 'POST',
+//                 body: formData
+//             });
+
+//             if (response.ok) {
+//                 let result = await response.json();
+//                 alert(result.message);
+//                 formPreview.innerHTML = '';
+//                 form.reset();
+//             } else {
+//                 alert('Problem');
+//             }
+//         } else {
+//             alert("Заповніть поля");
+//         }
+//     }
+
+//     function formValidate(form) {
+//         let error = 0;
+//         let formReq = form.querySelectorAll('._req');
+
+//         formReq.forEach(input => {
+//             formRemoveError(input);
+
+//             if (input.classList.contains('_email')) {
+//                 if (!emailTest(input.value)) {
+//                     formAddError(input);
+//                     error++;
+//                 }
+//             } else if (input.getAttribute("type") === "checkbox" && !input.checked) {
+//                 formAddError(input);
+//                 error++;
+//             } else {
+//                 if (input.value.trim() === '') {
+//                     formAddError(input);
+//                     error++;
+//                 }
+//             }
+//         });
+
+//         return error;
+//     }
+
+//     function formAddError(input) {
+//         input.parentElement.classList.add('_error');
+//         input.classList.add('_error');
+//     }
+
+//     function formRemoveError(input) {
+//         input.parentElement.classList.remove('_error');
+//         input.classList.remove('_error');
+//     }
+
+//     function emailTest(value) {
+//         return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(value);
+//     }
+// });
---
+++ php/sendmail.php
@@ -0,0 +1,44 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+
+if (isset($_POST['name']) && isset($_POST['email'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $body = $_POST['body-textarea'];
+}
+require_once "../phpmailer/PHPMailer-6.8.1/src/PHPMailer.php";
+require_once "../phpmailer/PHPMailer-6.8.1/src/SMTP.php";
+require_once "../phpmailer/PHPMailer-6.8.1/src/Exception.php";
+
+// require_once "../php/PHPMailer/PHPMailer.php";
+// require_once "PHPMailer/SMTP.php";
+// require_once "PHPMailer/Exception.php";
+
+    $mail = new PHPMailer();
+
+    // SMTP settings
+    $mail->isSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->Username = "martamykhailyna608@gmail.com";
+    $mail->Password = '608marta6008';
+    $mail->Port = 465;
+    $mail->SMTPSecure = "ssl";
+    // email settings
+    $mail->isHTML(true);
+    $mail->setFrom($email, $name);
+    $mail->addAddress("martamykhailyna608@gmail.com");
+    $mail->Subject = "$email($subject)";
+    $mail->Body = $body;
+
+    if ($mail->send()) {
+        $status = "success";
+        $response = "Email is sent!";
+    } else {
+        $status = "failed";
+        $response = "Something is wrong:<br>" . $mail->ErrorInfo;
+    }
+
+    exit(json_encode(array("status" => $status, "response" => $response)));
+    }
+?>
